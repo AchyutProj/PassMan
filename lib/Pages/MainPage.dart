13c2cf1aa2ca5f7b1b0b9f40975df3026a7554b5
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2023-08-19 11:41:15 +0545

    [PM-2] Initial Project Setup (#1)
    * [PM-3] Biolerplate code cleaned
    
    * [PM-4] Theme Setup
    
    * [PM-10] Splash Screen Page created
    
    With flutter_svg installed
    
    * [PM-5] Create Reusable PMAppBar Widget

---
+++ lib/Pages/MainPage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:passman/Components/PMAppBar.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PMAppBar(
+        title: 'Password Manager',
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/images/pass-man.svg', // Path to the SVG logo image
+              width: 100, // Adjust width as needed
+              height: 100, // Adjust height as needed
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to Passman',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your functionality here
+              },
+              child: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
