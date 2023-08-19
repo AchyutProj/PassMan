0f476308360e707e0dcd222adf31666d7dc1651b
Achyut Neupane <achyutkneupane@gmail.com>
2023-08-19 22:26:49 +0545

    [PM-22] Implement Conditional Navigation after Splash Screen

--- lib/Pages/splash_screen.dart
+++ lib/Pages/splash_screen.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:passman/Pages/main_page.dart';
 import 'package:passman/Pages/sign_up_page.dart';
+import 'package:passman/Utils/auth_service.dart';
 import 'package:passman/app_theme.dart';
-
-import 'main_page.dart'; // Update with your app's import
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final AuthService _authService = AuthService(); // Initialize your AuthService
+    final bool isLoggedIn = _authService.currentUser != null;
+
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const SignUpPage()), // Navigate to the main screen
+        MaterialPageRoute(
+          builder: (context) => isLoggedIn ? MainPage() : const SignUpPage(),
+        ),
       );
     });
+
     return Scaffold(
-      backgroundColor: AppTheme.primaryColor, // Set the background color
+      backgroundColor: AppTheme.primaryColor,
       body: Center(
         child: SvgPicture.asset(
           'assets/images/pass-man.svg',
-          width: 200, // Adjust width as needed
-          height: 200, // Adjust height as needed
+          width: 200,
+          height: 200,
         ),
       ),
     );
