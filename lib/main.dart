28dff869133ad4b6cffb0002fa7fd9087008bf1d
Achyut Neupane <achyutkneupane@gmail.com>
2023-08-19 10:08:58 +0545

    [PM-4] Theme Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:passman/app_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,17 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Password Manager App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'My Password Manager',
+      theme: AppTheme.themeData,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Password Manager'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Password Manager'),
-        ),
-        body: Center(
-          child: Text('Welcome to Password Manager App'),
-        ),
+      body: Center(
+        child: Text('Welcome to My Password Manager!'),
       ),
     );
   }
