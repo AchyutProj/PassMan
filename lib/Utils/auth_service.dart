e2a5d889cba3aacea1b51ddb65d8e6fdb9efe574
Achyut Neupane <achyutkneupane@gmail.com>
2023-08-19 21:37:04 +0545

    Error Exception updated

--- lib/Utils/auth_service.dart
+++ lib/Utils/auth_service.dart
@@ -7,25 +7,89 @@ class AuthService {
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
+  AuthStatus _status = AuthStatus.unknown;
+
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
 
-  Future<String?> signIn({required String email, required String password}) async {
+  Future<String> signIn({required String email, required String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-      return null;
+      _status = AuthStatus.successful;
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      _status = AuthExceptionHandler.handleAuthException(e);
     }
+    return AuthExceptionHandler.generateErrorMessage(_status);
   }
 
-  Future<String?> signUp({required String email, required String password}) async {
+  Future<String> signUp({required String email, required String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-      return null;
+      _status = AuthStatus.successful;
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      _status = AuthExceptionHandler.handleAuthException(e);
     }
+    return AuthExceptionHandler.generateErrorMessage(_status);
+  }
+}
+
+enum AuthStatus {
+  successful,
+  wrongPassword,
+  emailAlreadyExists,
+  invalidEmail,
+  weakPassword,
+  userNotFound,
+  unknown,
+}
+
+class AuthExceptionHandler {
+  static handleAuthException(FirebaseAuthException e) {
+    AuthStatus status;
+    switch (e.code) {
+      case "invalid-email":
+        status = AuthStatus.invalidEmail;
+        break;
+      case "wrong-password":
+        status = AuthStatus.wrongPassword;
+        break;
+      case "weak-password":
+        status = AuthStatus.weakPassword;
+        break;
+      case "email-already-in-use":
+        status = AuthStatus.emailAlreadyExists;
+        break;
+      case "user-not-found":
+        status = AuthStatus.invalidEmail;
+        break;
+      default:
+        status = AuthStatus.unknown;
+    }
+    return status;
+  }
+  static String generateErrorMessage(error) {
+    String errorMessage;
+    switch (error) {
+      case AuthStatus.invalidEmail:
+        errorMessage = "Your email address appears to be invalid.";
+        break;
+      case AuthStatus.weakPassword:
+        errorMessage = "Your password should be at least 6 characters.";
+        break;
+      case AuthStatus.wrongPassword:
+        errorMessage = "Your email or password is invalid.";
+        break;
+      case AuthStatus.emailAlreadyExists:
+        errorMessage =
+        "The email address is already in use by another account.";
+        break;
+      case AuthStatus.userNotFound:
+        errorMessage = "User not found.";
+        break;
+      default:
+        errorMessage = "An error occured. Please try again later.";
+    }
+    return errorMessage;
   }
 }
